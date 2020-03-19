a687fa41ac1a957348dc524874ccc58a51d7274b
dmitry1992.coding@gmail.com <dmitry1992.coding@gmail.com>
2020-03-19 11:53:37 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const array = str.split('');
+  const brackets = bracketsConfig.map((elem) => elem.join(''));
+  if (array.length % 2 !== 0) return false;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < brackets.length; j++) {
+      if (array[i] === brackets[j][0] && array[i+1] === brackets[j][1]) {
+        array.splice(i, 2);
+        i = -1;
+      }
+    }
+  }
+  return array.length === 0 ? true : false;
 }
